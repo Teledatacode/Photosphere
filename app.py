4efc6b6862fe37d24dd75a7a58919a9e835eaa0e
Teledatacode <appleroyale5689@gmail.com>
2025-07-06 23:21:37 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify 
 import cv2
 import numpy as np
 import base64
 from io import BytesIO
 from PIL import Image
+import os
 
 app = Flask(__name__)
 
@@ -51,4 +52,5 @@ def index():
     return "Stitching backend is running."
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
